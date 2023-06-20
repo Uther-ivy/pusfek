7e27836adcdfcc4172d19fb488b3f60f34937b6e
siku_spider <msnwow6688@hotmail.com>
2023-06-20 15:31:53 +0800

    Initial commit

--- 四库一平台/tyx_new_company.py
+++ 四库一平台/tyx_new_company.py
@@ -10,7 +10,7 @@ session = requests.session()
 def list_parse():
     dedup = RedisDeduplication()
 
-    for esdate in [["2020-06-20","2022-06-20"],["2018-06-20","2020-06-20"],["2013-06-20","2018-06-19"],["","2008-06-20"]]:#["2022-06-20","2023-06-20"],
+    for esdate in [["2018-06-20","2020-06-20"],["2013-06-20","2018-06-19"],["","2008-06-20"]]:#[["2022-06-20","2023-06-20"],["2020-06-20","2022-06-20"],]
         for page in range(1,501):
             print('*'*20,page,'*'*20)
             url = f'http://139.224.15.208/api/main-search/'
@@ -25,16 +25,18 @@ def list_parse():
                        "region": "110000",
                        "opscope": "",
                        "ltype": "",
-                       "page_size": 50,
+                       "page_size": 80,
                        "page_index": page}
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
             }
-            # data_list = session.get(url,headers=headers)
-            data_list = json.loads(session.post(url, headers=headers, data=payload).content.decode())
-            for db in data_list.get('data').get('list'):
-                print(db)
-                dedup.add('new_company_name', str(db.get('entname')))
-                dedup.add('new_company_organization', str(db.get('uniscid')))
-
+            res = session.post(url, headers=headers, data=payload)
+            if res.status_code==200:
+                data_list = json.loads(res.content.decode())
+                for db in data_list.get('data').get('list'):
+                    print(db)
+                    dedup.add('new_company_name', str(db.get('entname')))
+                    dedup.add('new_company_organization', str(db.get('uniscid')))
+            else:
+                break
 list_parse()
